481dc56ec32c878e3f7a0caa9af2b39b51a56509
JulioSaldias <juliosaldias05@gmail.com>
2024-08-15 22:27:09 -0400

    first commit

---
+++ vista/VCliente.php
@@ -0,0 +1,76 @@
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Lista de clientes registrados</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          <table id="example1" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Razon social</th>
+                <th>Nit/CI</th>
+                <th>Direccion</th>
+                <th>Nombre</th>
+                <th>Telefono - Email</th>
+                <td>
+                  <button class="btn btn-primary" onclick="MNuevoCliente()">Nuevo</button>
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              $cliente=ControladorCliente::ctrInfoClientes();
+              foreach($cliente as $value){
+              ?>
+
+              <tr>
+                <td><?php echo $value["id_cliente"];?></td>
+                <td><?php echo $value["razon_social_cliente"];?></td>
+                <td><?php echo $value["nit_ci_cliente"];?></td>
+                <td><?php echo $value["direccion_cliente"];?></td>
+                <td><?php echo $value["nombre_cliente"];?></td>
+                <td><?php echo $value["telefono_cliente"];?></td>
+                <td><?php echo $value["email_cliente"];?></td>
+
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-secondary" onclick="MEditCliente(<?php echo $value["id_cliente"];?>)">
+                      <i class="fas fa-edit"></i>
+                    </button>
+                    <button class="btn btn-danger" onclick="MEliCliente(<?php echo $value["id_cliente"];?>)">
+                      <i class="fas fa-trash"></i>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+
+              <?php
+              }
+              ?>
+            </tbody>
+
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
---
+++ vista/cliente/FNuevoCliente.php
@@ -0,0 +1,101 @@
+<form action="" id="FRegUsuario">
+  <div class="modal-header bg-primary">
+    <h4 class="modal-title">Registro nuevo cliente</h4>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+
+  <div class="modal-body">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="">Razon social</label>
+          <input type="text" class="form-control" name="rsocial" id="rsocial">
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="">Nit/CI</label>
+          <input type="text" class="form-control" name="nit" id="nit">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="">Direccion</label>
+          <input type="text" class="form-control" name="direccion" id="direccion">
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="">Nombre</label>
+          <input type="text" class="form-control" name="ncliente" id="ncliente">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="">Telefono</label>
+          <input type="text" class="form-control" name="telefono" id="telefono">
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label for="">Email</label>
+          <input type="text" class="form-control" name="email" id="email">
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <div class="modal-footer justify-content-between">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+    <button type="submit" class="btn btn-primary">Guardar</button>
+  </div>
+</form>
+
+<script>
+  $(function() {
+    $.validator.setDefaults({
+      submitHandler: function() {
+        regUsuario()
+      }
+    });
+
+    $('#FRegUsuario').validate({
+      rules: {
+        rsocial: {
+          required: true,
+          minlength: 5,
+        },
+        nit: {
+          required: true,
+          minlength: 7
+        },
+        nombre: {
+          required: true,
+          minlength: 5
+        },
+        emailbre: {
+          required: true,
+          email: true
+        },
+      },
+
+      errorElement: 'span',
+      errorPlacement: function(error, element) {
+        error.addClass('invalid-feedback');
+        element.closest('.form-group').append(error);
+      },
+      highlight: function(element, errorClass, validClass) {
+        $(element).addClass('is-invalid');
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        $(element).removeClass('is-invalid');
+      }
+    });
+  });
+</script>
